0dabdbee0bfcbc64c7ad0dcd245484df8e3468be
kirs333 <ghimirekrish479@gmail.com>
2025-12-24 16:40:49 +0545

    Queue operations and testing

--- queue.py
+++ queue.py
@@ -1,22 +1,30 @@
 class Queue:
-    def __init__(self):
-        self.items = []
-
-    def enqueue(self, item):
-        self.items.append(item)
-
-    def dequeue(self):
-        if not self.is_empty():
-            return self.items.pop(0)
-        return None
-
-    def peek(self):
-        if not self.is_empty():
-            return self.items[0]
-        return None
-
+    def __init__(self, size):
+        self.size = size
+        self.queue = []
+    
     def is_empty(self):
-        return len(self.items) == 0
-
-    def size(self):
-        return len(self.items)
+        return len(self.queue) == 0
+    
+    def is_full(self):
+        return len(self.queue) == self.size
+    
+    def enqueue(self, item):
+        if self.is_full():
+            print("Queue is full")
+            return
+        self.queue.append(item)
+    
+    def dequeue(self):
+        if self.is_empty():
+            print("Queue is empty")
+            return None
+        return self.queue.pop(0)
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.queue[0]
+    
+    def display(self):
+        print(self.queue)
